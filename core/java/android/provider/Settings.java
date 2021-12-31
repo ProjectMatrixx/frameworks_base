8349be2af6af4dc1c67947153d8a5613142eb26f
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-12-31 11:21:49 +0800

    base: Implement background process killer [1/2]
    * rebranded/adapted from Descendant-XI/frameworks_base@4d6a3fa
    * nuked stuff we dont want to mess with like sync and location
    
    Change-Id: I79c848dca348c18208a1a3a0de026fd9d80fbba2
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7438,6 +7438,13 @@ public final class Settings {
         public static final String EDGE_LIGHT_ANIMATION_EFFECT = "edge_light_animation_effect";
 
         /**
+         * Arcane Idle Manager
+         * @hide
+         */
+        @Readable
+        public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
