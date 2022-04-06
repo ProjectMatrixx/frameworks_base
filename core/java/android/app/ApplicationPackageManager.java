10025864b33816d4b76a9935acdad34cc840aeaa
Dylanger Daly <dylanger@diagnostix.io>
2022-04-06 11:58:34 +1000

    PM: Force all packages as installed via Google Play Store
    Change-Id: Ie561a52017630988570593291965fe6d67fa8527

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -2801,6 +2801,9 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public String getInstallerPackageName(String packageName) {
         try {
+            if (!packageName.equals("com.aurora.store") && packageName != null) {
+                return "com.android.vending";
+            }
             return mPM.getInstallerPackageName(packageName);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
