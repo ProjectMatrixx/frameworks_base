a5f2e9410a64879d5a1f82f9cc1cbbe4480b9578
Pranav Vashi <neobuddy89@gmail.com>
2024-08-21 00:28:48 +0530

    Add three fingers swipe actions [2/3]
    * Inspired by: https://github.com/RisingTechOSS/android_frameworks_base/commit/02780e27862edfa7c0b957073e9f4e1e4da2fb7d
    * Listener originally authored by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7162,6 +7162,14 @@ public final class Settings {
         public static final String ANBI_ENABLED = "anbi_enabled";
 
         /**
+         * Whether three fingers swipe is active
+         * 0 = Inactive, 1 = Active
+         * @hide
+         */
+        @Readable
+        public static final String THREE_FINGER_GESTURE_ACTIVE = "three_fingers_swipe_active";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
