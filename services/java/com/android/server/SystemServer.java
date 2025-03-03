8b01b3be51394a900ff7698e1d96033b8774c426
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-03 18:29:50 +0800

    Hide App list: Improve hide app list feature
    Insprired by: https://github.com/j-hc/zygisk-detach, instead of hooking libbinder, we override the computerengine to hide the app to the caller package. we should hide apps whereever possible except for system apps like launcher,settings, and framework. The main goal of Hide app list is to hide apps from playstore and banking apps, however we don't want to break package installer and system app list.
    
    Change-Id: I6d6cb159bde5dbabf37f313363996943ac881623
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2920,6 +2920,8 @@ public final class SystemServer implements Dumpable {
                 t.traceEnd();
             }
 
+            mSystemServiceManager.startService(HideAppListService.class);
+
             boolean hbmSupported = SystemProperties.getBoolean("persist.sys.hbmservice_support", false);
             String hbmFile = SystemProperties.get("persist.sys.hbmservice_file");
             if (hbmSupported && hbmFile != null && !hbmFile.isEmpty()) {
