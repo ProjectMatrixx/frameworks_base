9577ca66ab43a77c3c5ef30970fd9cee12fac40c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-04 11:39:52 +0000

    fixup! improve window ignore secure flag

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -1321,8 +1321,8 @@ public abstract class Window {
      * {@hide}
      */
     protected void dispatchWindowAttributesChanged(WindowManager.LayoutParams attrs) {
-        if ((attrs.flags & FLAG_SECURE) != 0 && Settings.Global.getInt(mContext.getContentResolver(),
-                "window_ignore_secure", 0) == 1) {
+        if (Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.WINDOW_IGNORE_SECURE, 0) == 1) {
             attrs.flags &= ~FLAG_SECURE;
         }
         if (mCallback != null) {
