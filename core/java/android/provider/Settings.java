56e263a8e72830c4fc07180f4b87e70c0933886f
darkphnx <dark.phnx12@gmail.com>
2025-08-17 13:13:24 +0000

    Revert "Revert "base: Add a toggle for long press on navbar to search [1/3]""
    This reverts commit c988bbe03a0bbe07ff58547f5805f0184bc4a3fc.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7418,6 +7418,13 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * Whether long press on navbar gesture is enabled
+         * @hide
+         */
+        @Readable
+        public static final String NAVBAR_LONG_PRESS_GESTURE = "navbar_long_press_gesture";
+
+        /**
          * Use doubletap as doze pulse triggers
          * @hide
          */
