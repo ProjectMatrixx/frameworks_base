2bd4967a2083c85e485ade2875b5036077c9fe60
darkphnx <dark.phnx12@gmail.com>
2025-08-18 16:11:49 +0000

    Revert "ClockEventController: fix clock plugins animation"
    This reverts commit bf9a69200dfd0c9b6a83260384926c49c17c9219.

--- packages/SystemUI/src/com/android/keyguard/ClockEventController.kt
+++ packages/SystemUI/src/com/android/keyguard/ClockEventController.kt
@@ -469,7 +469,7 @@ constructor(
         zenModeController.addCallback(zenModeCallback)
         if (SceneContainerFlag.isEnabled) {
             handleDoze(
-                when (DOZING) {
+                when (AOD) {
                     keyguardTransitionInteractor.getCurrentState() -> 1f
                     keyguardTransitionInteractor.getStartedState() -> 1f
                     else -> 0f
@@ -604,10 +604,10 @@ constructor(
     internal fun listenForDozeAmountTransition(scope: CoroutineScope): Job {
         return scope.launch {
             merge(
-                    keyguardTransitionInteractor.transition(Edge.create(DOZING, LOCKSCREEN)).map {
+                    keyguardTransitionInteractor.transition(Edge.create(AOD, LOCKSCREEN)).map {
                         it.copy(value = 1f - it.value)
                     },
-                    keyguardTransitionInteractor.transition(Edge.create(LOCKSCREEN, DOZING)),
+                    keyguardTransitionInteractor.transition(Edge.create(LOCKSCREEN, AOD)),
                 )
                 .filter { it.transitionState != TransitionState.FINISHED }
                 .collect { handleDoze(it.value) }
@@ -621,7 +621,7 @@ constructor(
     internal fun listenForAnyStateToAodTransition(scope: CoroutineScope): Job {
         return scope.launch {
             keyguardTransitionInteractor
-                .transition(Edge.create(to = DOZING))
+                .transition(Edge.create(to = AOD))
                 .filter { it.transitionState == TransitionState.STARTED }
                 .filter { it.from != LOCKSCREEN }
                 .collect { handleDoze(1f) }
@@ -634,7 +634,7 @@ constructor(
             keyguardTransitionInteractor
                 .transition(Edge.create(to = LOCKSCREEN))
                 .filter { it.transitionState == TransitionState.STARTED }
-                .filter { it.from != DOZING }
+                .filter { it.from != AOD }
                 .collect { handleDoze(0f) }
         }
     }
