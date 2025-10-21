1070d82daea124d275757338898b82ff2a57b5ee
Ghosuto <clash.raja10@gmail.com>
2025-10-21 11:39:29 +0000

    SystemUI: Add custom udfps fp icon support
    Change-Id: Ia5044a111d3ac112f2904108ca7dd4d797bb86db
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7122,6 +7122,16 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ICON_TYPE = "udfps_icon_type";
+
+	/**
+         * @hide
+         */
+        public static final String UDFPS_CUSTOM_FP_ICON_PATH = "udfps_custom_fp_icon_path";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
