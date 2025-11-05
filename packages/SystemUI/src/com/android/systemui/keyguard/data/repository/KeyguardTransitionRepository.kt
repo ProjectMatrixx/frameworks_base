835b85bf8c28e60eac22e3bed174362c1f727f33
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-05 15:41:45 +0800

    SystemUI: Fix keyguard transition race conditions
    exasperated sigh*  google ☕ ..
    
    Change-Id: I7aafc191af478d069921f41d7e21c133a33ad85d
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardTransitionRepository.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardTransitionRepository.kt
@@ -212,9 +212,9 @@ constructor(
 
         // Animators must be started on the main thread.
         return withContext("$TAG#startTransition", mainDispatcher) {
-            withContextMutex.unlock()
             if (lastStep.from == info.from && lastStep.to == info.to) {
                 Log.i(TAG, "Duplicate call to start the transition, rejecting: $info")
+                withContextMutex.unlock()
                 return@withContext null
             }
             val isAnimatorRunning = lastAnimator?.isRunning == true
@@ -298,6 +298,7 @@ constructor(
                 animator.addListener(animatorListener)
                 animator.addUpdateListener(updateListener)
                 animator.start()
+                withContextMutex.unlock()
                 return@withContext null
             }
                 ?: run {
@@ -308,6 +309,7 @@ constructor(
 
                     // No animator, so it's manual. Provide a mechanism to callback
                     updateTransitionId = UUID.randomUUID()
+                    withContextMutex.unlock()
                     return@withContext updateTransitionId
                 }
         }
