3907d12a74dc5f456e367056e0e743ccdc69c59e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2026-01-30 00:35:56 +0100

    base: Introduce Smart 5G service [1/2]
    [nurkeinneid]
    - New service class under com.android.server.telephony (no libremobileos package)
    - Use Context.RECEIVER_NOT_EXPORTED when registering the broadcast receiver (recommended on API 33+)
    - In isMobileDataNetwork(), add a null check for getNetworkCapabilities(network) to avoid an NPE
    - Replace the switch (action) with a null check on action to satisfy null-safety
    
    Change-Id: Ia075446a3e111a07814b7e5f2d2e7e8fbe737475
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -288,6 +288,7 @@ import com.android.server.storage.DeviceStorageMonitorService;
 import com.android.server.supervision.SupervisionService;
 import com.android.server.systemcaptions.SystemCaptionsManagerService;
 import com.android.server.telecom.TelecomLoaderService;
+import com.android.server.telephony.Smart5gService;
 import com.android.server.testharness.TestHarnessModeService;
 import com.android.server.textclassifier.TextClassificationManagerService;
 import com.android.server.textservices.TextServicesManagerService;
@@ -2907,6 +2908,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("GameSpaceManagerService");
             mSystemServiceManager.startService(GameSpaceManagerService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartSmart5gService");
+            mSystemServiceManager.startService(Smart5gService.class);
             t.traceEnd();
 
             if (context.getResources().getBoolean(R.bool.config_dozeAlwaysOnDisplayAvailable)) {
