1ee478e472245b80dfcf0e064e7bc551cb76fba3
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-02-17 06:40:15 +0800

    SystemUI: Adding platform hooks
    adding hooks to reduce ax platform apps boilerplate
    
    [neobuddy89: Hook in SystemUICoreStartableModule.]
    
    Change-Id: Ibdbd59a11ea8375c8698bef65a971a6c522fbe3e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -21,6 +21,7 @@ import com.android.systemui.CoreStartable
 import com.android.systemui.LatencyTester
 import com.android.systemui.SliceBroadcastRelayHandler
 import com.android.systemui.accessibility.Magnification
+import com.android.systemui.ax.AxPlatformServiceImpl
 import com.android.systemui.back.domain.interactor.BackActionInteractor
 import com.android.systemui.biometrics.BiometricNotificationService
 import com.android.systemui.bouncer.domain.startable.BouncerStartable
@@ -343,4 +344,8 @@ abstract class SystemUICoreStartableModule {
     @ClassKey(UsbModePickerDialogDelegate::class)
     abstract fun bindUsbModePickerDialogDelegate(impl: UsbModePickerDialogDelegate): CoreStartable
 
+    @Binds
+    @IntoMap
+    @ClassKey(AxPlatformServiceImpl::class)
+    abstract fun bindAxPlatformService(impl: AxPlatformServiceImpl): CoreStartable
 }
