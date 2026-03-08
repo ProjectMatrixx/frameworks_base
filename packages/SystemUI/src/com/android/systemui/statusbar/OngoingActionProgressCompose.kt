a6f7067c808a572f76aa09ecaa05f11b6073ed8c
Ghosuto <clash.raja10@gmail.com>
2026-03-08 05:56:40 +0000

    SystemUI: Add background to ongoing progress chip
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/OngoingActionProgressCompose.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/OngoingActionProgressCompose.kt
@@ -183,10 +183,14 @@ fun OngoingActionProgress(
 
             else -> {
                 val pv = progressFraction(state)
+                val chipBg = colorResource(android.R.color.system_accent1_500)
                 Row(
                     modifier = Modifier
                         .width(86.dp).height(26.dp)
-                        .padding(horizontal = 6.dp, vertical = 4.dp)
+                        .padding(start = 4.dp)
+                        .clip(chipShape)
+                        .background(chipBg)
+                        .padding(horizontal = 5.dp, vertical = 3.dp)
                         .then(gestureModifier),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
